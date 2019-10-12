7fe17cff834761ed99baea9b3365614ce4149195
S Biswas <sbiswas1975@gmail.com>
2019-10-12 19:57:04 +0530

    Code changes for new element addition

--- assets/javascript/Categories.js
+++ assets/javascript/Categories.js
@@ -1,23 +1,17 @@
 
-var Ctg=Array();
+var topic=Array();
 
 function show(){
-Ctg = ['dog', 'cat', 'rabbit', 'hamster', 'skunk', 'goldfish', 'bird', 'frog'];
-var ctgButtons = $('#ctgButtons');
-for (var i = 0; i < Ctg.length; i++) {
-  ctgButtons.append('<button type="submit" id="' + Ctg[i] + '" name="' + Ctg[i] + '" class="btn">' + Ctg[i] + '</button>');
-}
+topic = ['dog', 'cat', 'rabbit', 'hamster', 'skunk', 'goldfish', 'bird', 'frog'];
+var topicButtons = $('#topicButtons');
+for (var i = 0; i < topic.length; i++) {
+  topicButtons.append('<button type="submit" id="' + topic[i] + '" name="' + topic[i] + '" class="btn">' + topic[i] + '</button>');
+  }
 }
 
-//var data = new Array(); 
 function add_element(){
-Ctg.push(document.getElementById('t1').value); // adding element to array
-
-var pval="";
-for(i=0; i<Ctg.length; i++)
-{
-	pval=pval+Ctg[i];
-}
-document.getElementById('t1').value=''; // Making the text box blank
-show(); // displaying the array elements
+  var newTopic=document.getElementById('newTopicVal').value;
+  if()
+  var topicButtons = $('#topicButtons');
+  topicButtons.append('<button type="submit" id="' + newTopic + '" name="' + newTopic + '" class="btn">' + newTopic + '</button>');
 }
