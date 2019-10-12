a6e4733cddcd8cd82fefd844674dbb90d99da916
S Biswas <sbiswas1975@gmail.com>
2019-10-13 00:38:31 +0530

    code change

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,8 @@
 $(document).on('click','.btn', function()
 {
 
+var gifStill="";
+var gifUrl="";
 var selectedid=$(this).attr("id");    
 
 var $form=$("form"),
@@ -14,13 +16,15 @@ $form.on("submit", function(e) {
 
 function goGiphy() {
 	var input=$search;
-	var api_url="https://api.giphy.com/v1/gifs/search?limit=10";
-	var apiKey="&api_key=9iG24Ub5gqbw0Xyp8WKn0M3oe40pr5J9";
+	var api_url="https://api.giphy.com/v1/gifs/search?";
+	var apiKey="api_key=9iG24Ub5gqbw0Xyp8WKn0M3oe40pr5J9";
 	var query="&q="+input;
 	
 	$.getJSON(api_url+apiKey+query, function(json){
 		data=JSON.parse(JSON.stringify(json));
 		var output="";
+
+		document.cookie = "testing";
 		
 		for(var i=0; i< data.data.length; i++){
 		var gifObj=data.data[i];
@@ -31,10 +35,13 @@ function goGiphy() {
 		gifTitle=gifObj.title;
 
 		
+		//$.cookie('description',data);
+
+		
 		console.log(gifUrl)
 		//output+="<div class='col-sm-3'>Rating: "+gifRating+"</div><br/><img id='"+gifId+"' onclick='changeImage("+gifId+")' style='cursor:pointer;' class='col-sm-3' width='100px' height='150px' src='"+gifStill+"'/>";
 		output+="<div class='col-sm-3 giphyStill'><div class='giphyrating'><span class='giphyHeader'>Title: "+gifTitle+"</span><br/><span class='giphyHeader'>Rating: "+gifRating+"</span></div><img id='"+gifId+"' style='cursor:pointer;' class='img-responsive' style='width:225px! Important;' alt='"+gifStill+"' src='"+gifStill+"'></div>";
-
+		
 	}
 	console.log(data)
     $("#gifhy").html(output);
@@ -51,6 +58,11 @@ $(document).on('click','.img-responsive', function()
 	var queryid=this.id;
 	var imgId="#"+queryid;
 	var animUrl;
+
+	console.log(document.cookie);
+	/*for(var i=0; i< data.data.length; i++){
+		var gifObj=data.data[i];
+	}*/
 	
 	$.getJSON(api_url+queryid+apiKey, function(json){
 		dataurl=JSON.parse(JSON.stringify(json));
