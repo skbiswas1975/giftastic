fe5650892c74d663eef0389590e1915c044a3deb
S Biswas <sbiswas1975@gmail.com>
2019-10-14 00:03:46 +0530

    functionality excluded

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,3 @@
-
 var counter=0; 
 var output="";
 var searchTopic="";
@@ -6,54 +5,40 @@ var searchTopic="";
 
 $(document).on('click','.btn', function()
 {
-	
+
+	//if(document.cookie)
 var gifStill="";
 var gifUrl="";
-var selectedid=$(this).attr("id");  
-console.log(selectedid);
-console.log(output);
+var selectedid=$(this).attr("id");   
 
-searchTopic=getCookie("SearchTopic");
-
-console.log(getCookie("SearchTopic"));
-
-if(searchTopic !=selectedid)
+if(document.cookie["SearchTopic"] !=selectedid)
 {
 	var counter=0; 
 	var output="";
 	var searchTopic="";
 }
 
-else
-{
-	counter=getCookie("counter");
-}
 
-var $form=$("form");
-	//$search=$('#'+selectedid+'').text();
+
+var $form=$("form"),
+	$search=$('#'+selectedid+'').text();
 	
 $form.on("submit", function(e) {
 	e.preventDefault();
-	console.log(selectedid);
 	goGiphy();
 });
 
 function goGiphy() {
 
-	console.log(selectedid);
 	//var counter=0; 
 	//var output="";
-	//searchTopic=$search;
-	searchTopic=selectedid;
+	searchTopic=$search;
 	var input=searchTopic;
 	var api_url="https://api.giphy.com/v1/gifs/search?limit=10&offset="+counter;
 	var apiKey="&api_key=9iG24Ub5gqbw0Xyp8WKn0M3oe40pr5J9";
 	var query="&q="+input;
 
 	counter=counter+10;
-	
-	setCookie("counter", counter, 1);
-	setCookie("SearchTopic", searchTopic, 1);
 
 	console.log(document.cookie);
 
@@ -75,7 +60,7 @@ function goGiphy() {
 		console.log(gifUrl)
 		//output+="<div class='col-sm-3'>Rating: "+gifRating+"</div><br/><img id='"+gifId+"' onclick='changeImage("+gifId+")' style='cursor:pointer;' class='col-sm-3' width='100px' height='150px' src='"+gifStill+"'/>";
 		output+="<div class='col-sm-3 giphyStill'><div class='giphyrating'><span class='giphyHeader'>Title: "+gifTitle+"</span><br/><span class='giphyHeader'>Rating: "+gifRating+"</span></div><img id='"+gifId+"' style='cursor:pointer;' class='img-responsive' style='width:225px! Important;' alt='"+gifStill+"' src='"+gifStill+"'></div>";
-		console.log($(this).attr("id"));
+		
 	}
 	console.log(data)
     $("#gifhy").html(output);
@@ -83,28 +68,6 @@ function goGiphy() {
 }
 
 });
-
-function setCookie(name, value, days) {
-    var d = new Date;
-    d.setTime(d.getTime() + 24*60*60*1000*days);
-    document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
-}
-
-function getCookie(cname) {
-	var name = cname + "=";
-	var decodedCookie = decodeURIComponent(document.cookie);
-	var ca = decodedCookie.split(';');
-	for(var i = 0; i <ca.length; i++) {
-	  var c = ca[i];
-	  while (c.charAt(0) == ' ') {
-		c = c.substring(1);
-	  }
-	  if (c.indexOf(name) == 0) {
-		return c.substring(name.length, c.length);
-	  }
-	}
-	return "";
-  }
 
 
 $(document).on('click','.img-responsive', function()
@@ -127,7 +90,7 @@ $(document).on('click','.img-responsive', function()
 
 	   if($(imgId).attr("src")==stillGif)
 		{
-			alert("Please wait while animated gif is loading!");
+			alert("Please wait while image is loading!");
 			$(imgId).attr("src", animGif);
 		}
 		else
