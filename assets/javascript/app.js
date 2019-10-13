c3356a32ae808cefc4be396e51112b37acf94dda
S Biswas <sbiswas1975@gmail.com>
2019-10-13 13:50:52 +0530

    Completed single api request instead of 2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,7 @@ $form.on("submit", function(e) {
 
 function goGiphy() {
 	var input=$search;
-	var api_url="https://api.giphy.com/v1/gifs/search?limit=2";
+	var api_url="https://api.giphy.com/v1/gifs/search?limit=10";
 	var apiKey="&api_key=9iG24Ub5gqbw0Xyp8WKn0M3oe40pr5J9";
 	var query="&q="+input;
 	
@@ -27,9 +27,6 @@ function goGiphy() {
     	var myCookie = JSON.stringify(data);
 		document.cookie = "result="+myCookie;
 		console.log(document.cookie);
-
-		//$.cookie('user', myCookie);
-		//console.log(document.cookie);
 		
 		for(var i=0; i< data.data.length; i++){
 		var gifObj=data.data[i];
@@ -39,10 +36,6 @@ function goGiphy() {
 		gifRating=gifObj.rating;
 		gifTitle=gifObj.title;
 
-		
-		//$.cookie('description',data);
-
-		
 		console.log(gifUrl)
 		//output+="<div class='col-sm-3'>Rating: "+gifRating+"</div><br/><img id='"+gifId+"' onclick='changeImage("+gifId+")' style='cursor:pointer;' class='col-sm-3' width='100px' height='150px' src='"+gifStill+"'/>";
 		output+="<div class='col-sm-3 giphyStill'><div class='giphyrating'><span class='giphyHeader'>Title: "+gifTitle+"</span><br/><span class='giphyHeader'>Rating: "+gifRating+"</span></div><img id='"+gifId+"' style='cursor:pointer;' class='img-responsive' style='width:225px! Important;' alt='"+gifStill+"' src='"+gifStill+"'></div>";
@@ -58,8 +51,6 @@ function goGiphy() {
 
 $(document).on('click','.img-responsive', function()
 {
-	//var api_url="https://api.giphy.com/v1/gifs/";
-	//var apiKey="?api_key=9iG24Ub5gqbw0Xyp8WKn0M3oe40pr5J9";
 	var queryid=this.id;
 	var imgId="#"+queryid;
 	var animGif;
@@ -69,7 +60,7 @@ $(document).on('click','.img-responsive', function()
 
 		var singleData = data.data[index];
 	   
-		if(singleData.id == imgId){
+		if(singleData.id == queryid){
 			animGif=singleData.images.original.url;
 			stillGif=singleData.images.original_still.url;
 			break;
@@ -84,23 +75,4 @@ $(document).on('click','.img-responsive', function()
 		{
 			$(imgId).attr("src", stillGif);
 		}
-
-	/*
-	$.getJSON(api_url+queryid+apiKey, function(json){
-		dataurl=JSON.parse(JSON.stringify(json));
-		
-		stillUrl=dataurl.data.images.original_still.url;
-		animUrl=dataurl.data.images.original.url;
-		
-		
-		if($(imgId).attr("src")==stillUrl)
-		{
-			$(imgId).attr("src", animUrl);
-		}
-		else
-		{
-			$(imgId).attr("src", stillUrl);
-		}
-	})
-	*/
 });
